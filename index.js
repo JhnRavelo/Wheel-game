fa925a0555f32b5c5ea356a35dbf39dd55bdaeda
JhnRavelo <johnravelo135@gmail.com>
2023-11-23 08:18:29 +0300

    Game over

--- index.js
+++ index.js
@@ -1,38 +1,65 @@
 let container = document.querySelector(".container");
-const modal = document.querySelector(".game-modal")
-const gameModal = document.querySelector(".game-modal")
+const modal = document.querySelector(".game-modal");
+const gameModal = document.querySelector(".game-modal");
+const btnTryAgain = document.querySelector(".play-again");
 let btn = document.querySelector(".game");
-var number = 2000
-var currentWord
+var number = 2000;
+var currentWord;
 btn.onclick = function () {
-	container.style.transform = "rotate(" + number + "deg)";
-	number += 2000;
-}
+  container.style.transform = "rotate(" + number + "deg)";
+  number += 2000;
+};
 
-const gift = ["-6%","-5%","-4%","-3%","-2%","-1%","0%"]
+const gift = ["-6%", "-5%", "-4%", "-3%", "-2%", "-1%", "0%"];
 
 const gameOver = (isVictory) => {
-    // After game complete.. showing modal with relevant details
-    const modalText = isVictory ? `Vous avez gagnez <b>${currentWord}</b>` : 'Vous avez perdu';
-    console.log(modalText)
-    gameModal.querySelector(".img").src = `images/${isVictory ? 'fireworks' : 'lost'}.gif`;
-    gameModal.querySelector("h4").innerText = isVictory ? 'Felicitations!' : 'Game Over!';
-    gameModal.querySelector("p").innerHTML = `${modalText}`;
-    gameModal.classList.add("show");
-}
+  // After game complete.. showing modal with relevant details
+  const modalText = isVictory
+    ? `Vous avez gagnez <b>${currentWord}</b>`
+    : "Vous avez perdu";
+  console.log(modalText);
+  gameModal.querySelector(".img").src = `images/${
+    isVictory ? "fireworks" : "lost"
+  }.gif`;
+  gameModal.querySelector("h4").innerText = isVictory
+    ? "Felicitations!"
+    : "Dommage!";
+  gameModal.querySelector("p").innerHTML = `${modalText}`;
+  gameModal.classList.add("show");
+};
 
 const runWheel = () => {
-    const rdmNumber = Math.random
-    if(rdmNumber<1/30){
-        currentWord = gift[5]
-    }
-    else if(rdmNumber<1/35){
-        currentWord = gift[4]
-    }
-}
+  const rdmNumber = Math.random();
+  
+  if (rdmNumber < 1 / 4 && rdmNumber > 1 / 5) {
+    currentWord = gift[5];
+  } else if (rdmNumber < 1 / 5 && rdmNumber > 1 / 6) {
+    currentWord = gift[4];
+  } else if (rdmNumber < 1 / 6 && rdmNumber > 1 / 7) {
+    currentWord = gift[3];
+  } else if (rdmNumber < 1 / 7 && rdmNumber > 1 / 8) {
+    currentWord = gift[2];
+  } else if (rdmNumber < 1 / 8 && rdmNumber > 1 / 9) {
+    currentWord = gift[1];
+  } else if (rdmNumber < 1 / 9) {
+    currentWord = gift[0];
+  } else {
+    currentWord = gift[6];
+  }
+  return currentWord;
+};
 
-container.addEventListener("transitionend", ()=>{
-    runWheel()
-    gameOver(true)
-    modal.classList.add("show")
-})
+container.addEventListener("transitionend", () => {
+  const result = runWheel();
+  console.log(result);
+  if (result != "0%") {
+    gameOver(true);
+  } else {
+    gameOver(false);
+  }
+  modal.classList.add("show");
+});
+
+btnTryAgain.onclick = () => {
+  modal.classList.remove("show");
+};
